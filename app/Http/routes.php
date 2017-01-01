765b00f64b2e811db5fec7d14260141a3f498275
RB <RB>
2017-01-02 02:47:26 +0800

    setting up user log in

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -14,10 +14,18 @@
 Route::group(['middleware' => ['web']], function () {
 Route::get('/', function () {
     return view('welcome');
-});
+	});
 
 	Route::post('/signup', [
 		'uses' => 'UserController@postSignUp',
 		'as' => 'signup'
+	]);
+	Route::post('/signin', [
+		'uses' => 'UserController@postSignIn',
+		'as' => 'signin'
+	]);
+	Route::get('/dashboard', [
+		'uses' => 'UserController@getDashboard',
+		'as' => 'dashboard'
 	]);
 });
