21dbbd635aed9b224eda3497d7b225c5fc898574
RB <RB>
2017-01-02 01:29:52 +0800

    setting up signup controller and model

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -11,6 +11,13 @@
 |
 */
 
+Route::group(['middleware' => ['web']], function () {
 Route::get('/', function () {
     return view('welcome');
+});
+
+	Route::post('/signup', [
+		'uses' => 'UserController@postSignUp',
+		'as' => 'signup'
+	]);
 });
