e7ec041a34008460504014f86dba95c53401d162
RB <RB>
2017-01-02 22:53:38 +0800

    worked with editing posts with ajax request

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -44,3 +44,11 @@ Route::group(['middleware' => ['web']], function () {
 		'as' => 'post.delete',
 		'middleware' => 'auth'
 	]);
+	Route::post('/edit', function(\Illuminate\Http\Request $request) {
+		return response()->json(['message' => $request['postId']]);
+		/*
+		 * {
+		 * 		message: '$request['body']'
+		 * }
+		 */
+	})->name('edit');
