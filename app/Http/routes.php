8fae83ff7dc0f90185fbc41cd3f93adc45f9458e
RB <RB>
2017-01-03 00:16:54 +0800

    worked with user account

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -28,7 +28,14 @@ Route::group(['middleware' => ['web']], function () {
 		'uses' => 'UserController@getLogout',
 		'as' => 'logout'
 	]);
-
+	Route::get('/account', [
+		'uses' => 'UserController@getAccount',
+		'as'=> 'account'
+	]);
+	Route::post('/updateaccount', [
+		'uses' => 'UserController@postSaveAccount',
+		'as' => 'account.save'
+	]);
 	Route::get('/dashboard', [
 		'uses' => 'PostController@getDashboard',
 		'as' => 'dashboard',
